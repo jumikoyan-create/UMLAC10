373f5c36084c1c91e3b3604159177c04a4d2e854
jumikoyan-create <jumikoyan@gmail.com>
2026-04-30 00:10:31 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module co.edu.poli.actividad10 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens co.edu.poli.contexto3.controlador to javafx.fxml;
-    exports co.edu.poli.contexto3.vista;
-    
-    opens co.edu.poli.contexto3.modelo to javafx.base;
-}
